36ded2bb34a2cee1bc4945e0ce11c9fcf0284f58
Marcin N <mnawrocki81@gmail.com>
2019-07-17 23:36:32 +0200

    Dodanie radiobutton grawer, sprawdzanie poprawności znaków, cofnięcie wprowadzonego tekstu

--- NiesmiertelnikText/Form1.Designer.cs
+++ NiesmiertelnikText/Form1.Designer.cs
@@ -28,21 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.radioButtonNiesmiertelnik = new System.Windows.Forms.RadioButton();
+            this.radioButtonGrawer = new System.Windows.Forms.RadioButton();
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.richTextBox1.Location = new System.Drawing.Point(33, 66);
+            this.richTextBox1.Location = new System.Drawing.Point(16, 51);
             this.richTextBox1.MaxLength = 1000;
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(191, 112);
+            this.richTextBox1.Size = new System.Drawing.Size(365, 132);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -51,7 +57,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(29, 281);
+            this.label1.Location = new System.Drawing.Point(12, 284);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(13, 20);
             this.label1.TabIndex = 2;
@@ -61,11 +67,13 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(33, 200);
+            this.button1.Location = new System.Drawing.Point(190, 200);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 50);
             this.button1.TabIndex = 3;
             this.button1.Text = "Ile znaków?";
+            this.toolTip2.SetToolTip(this.button1, "Przycisk oblicza ilośc znaków i sprawdza czy nie ma znaków niedozwolonych.\r\nW prz" +
+        "ypadku nieśmiertelników zmienia znaki na duże i zamienia polskie na angielskie!");
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -73,17 +81,17 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(29, 43);
+            this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(204, 20);
+            this.label2.Size = new System.Drawing.Size(265, 20);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Wpisz tekst nieśmiertelnika:";
+            this.label2.Text = "Wpisz tekst nieśmiertelnika/graweru:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button3.Location = new System.Drawing.Point(156, 200);
+            this.button3.Location = new System.Drawing.Point(313, 200);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(68, 49);
             this.button3.TabIndex = 6;
@@ -91,16 +99,52 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // radioButtonNiesmiertelnik
+            // 
+            this.radioButtonNiesmiertelnik.AutoSize = true;
+            this.radioButtonNiesmiertelnik.Checked = true;
+            this.radioButtonNiesmiertelnik.Location = new System.Drawing.Point(16, 200);
+            this.radioButtonNiesmiertelnik.Name = "radioButtonNiesmiertelnik";
+            this.radioButtonNiesmiertelnik.Size = new System.Drawing.Size(151, 17);
+            this.radioButtonNiesmiertelnik.TabIndex = 7;
+            this.radioButtonNiesmiertelnik.TabStop = true;
+            this.radioButtonNiesmiertelnik.Text = "Nieśmiertelnik (15 znaków)";
+            this.radioButtonNiesmiertelnik.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonGrawer
+            // 
+            this.radioButtonGrawer.AutoSize = true;
+            this.radioButtonGrawer.Location = new System.Drawing.Point(16, 223);
+            this.radioButtonGrawer.Name = "radioButtonGrawer";
+            this.radioButtonGrawer.Size = new System.Drawing.Size(120, 17);
+            this.radioButtonGrawer.TabIndex = 8;
+            this.radioButtonGrawer.Text = "Grawer (28 znaków)";
+            this.radioButtonGrawer.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 466);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Cofnij";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(403, 501);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButtonGrawer);
+            this.Controls.Add(this.radioButtonNiesmiertelnik);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Nieśmiertelnik Tekst";
             this.ResumeLayout(false);
@@ -115,6 +159,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButtonNiesmiertelnik;
+        private System.Windows.Forms.RadioButton radioButtonGrawer;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- NiesmiertelnikText/Form1.cs
+++ NiesmiertelnikText/Form1.cs
@@ -4,19 +4,23 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Collections;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace NiesmiertelnikText
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        string previusText = "";
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -25,40 +29,46 @@ namespace NiesmiertelnikText
 
         private void button1_Click(object sender, EventArgs e)
         {
-            richTextBox1.Text = ChangeText(richTextBox1.Text);
 
-            int count = richTextBox1.Lines.Count();
             label1.Text = "";
 
-            for (int i = 0; i < count; i++)
+            if (radioButtonNiesmiertelnik.Checked)
             {
 
-                label1.Text += ($"W {i + 1} linii jest {richTextBox1.Lines[i].Length} znaków. ");
-
-                if (richTextBox1.Lines[i].Length > 15)
-                    label1.Text += "ZA DUŻO ZNAKÓW!!!";
-
-                label1.Text += ("\n");
-
+               previusText = richTextBox1.Text;
+               richTextBox1.Text = ChangeText(richTextBox1.Text);
+               NiesmiertelnikCount();
+               CheckText();
+               
             }
 
+            if (radioButtonGrawer.Checked)
+            {
+                previusText = richTextBox1.Text;
+                richTextBox1.Text = richTextBox1.Text;
+                GrawerCount();
+                CheckText();
+                
+            }
         }
-
 
 
         private string ChangeText(string text)
         {
             string newtext = text.ToUpper();
-            string[,] polskieznaki = {
+            string[,] polishmarks = {
           { "Ą", "A" }, { "Ć", "C" }, { "Ę", "E" }, { "Ł", "L" }, { "Ń", "N" },
-          { "Ó", "O" }, { "Ś", "S" }, { "Ź", "Z" }, { "Ż", "Z" }, { "Ü", "U" } };
-            for (int i = 0; i < polskieznaki.GetLength(0); i++)
+          { "Ó", "O" }, { "Ś", "S" }, { "Ź", "Z" }, { "Ż", "Z" }, { "Ü", "U" },
+          { "Ä", "A" }, { "Ö", "O" } };
+          
+            for (int i = 0; i < polishmarks.GetLength(0); i++)
             {
-                newtext = newtext.Replace(polskieznaki[i, 0], polskieznaki[i, 1]);
+                newtext = newtext.Replace(polishmarks[i, 0], polishmarks[i, 1]);
             }
 
             return newtext;
         }
+
 
         private void label2_Click(object sender, EventArgs e)
         {
@@ -69,11 +79,129 @@ namespace NiesmiertelnikText
         {
 
         }
-
+        
         private void button3_Click(object sender, EventArgs e)
         {
             richTextBox1.Text = "";
             label1.Text = "";
         }
+
+
+        private void CheckText ()
+        {
+            
+            string signs = "";
+            string letters = "";
+
+            ArrayList badSign = new ArrayList();
+
+            if (radioButtonNiesmiertelnik.Checked)
+            {
+                letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                signs = "!()?.,'@~ &*:/\"\n+-";
+            }
+            else
+            {   letters = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSTUVWXYZŻŹ";
+                signs = "!()?.,'@~ &*:/\"\n+-%";
+            }
+
+            string numbers = "0123456789";
+
+            for (int i = 0; i < richTextBox1.Text.Length; i++)
+            {
+               bool goodSign = false;
+
+                for (int j = 0; j < signs.Length; j++)
+                {
+                    if (richTextBox1.Text[i].Equals(signs[j]))
+                    {
+                        goodSign = true;
+                        break;
+                    }
+                }
+
+                if (goodSign==false)
+                {
+                    for (int j = 0; j < letters.Length; j++)
+                    {
+                        if (string.Equals(richTextBox1.Text[i].ToString(),letters[j].ToString(), StringComparison.OrdinalIgnoreCase))
+                        {
+                            goodSign = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (goodSign == false)
+                {
+                    for (int j = 0; j < numbers.Length; j++)
+                    {
+                        if (richTextBox1.Text[i].Equals(numbers[j]))
+                        {
+                            goodSign = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (goodSign == false)
+                {
+                    badSign.Add(i);
+                    continue;
+                }
+            }
+
+           if (badSign.Count>0)
+           MessageBox.Show("Tekst zawiera niedozwolne znaki! Popraw podświetlone i zaznaczone na czerwono znaki!");
+
+           foreach (int sign in badSign)
+            {
+                richTextBox1.Select(sign, 1);
+                richTextBox1.SelectionColor = Color.Red;
+                richTextBox1.SelectionBackColor = Color.White;
+            }
+        }
+
+        private void GrawerCount()
+        {
+            int count = richTextBox1.Lines.Count();
+
+            label1.Text = ($"W linii jest {richTextBox1.Lines[0].Length} znaków. ");
+
+            if (richTextBox1.Lines[0].Length > 28)
+                label1.Text += "ZA DUŻO ZNAKÓW!!!";
+
+            if (count > 1)
+                label1.Text += ("\nDOZWOLONA MAX. 1 LINIA! POPRAW TEKST!");
+        }
+
+        private void NiesmiertelnikCount()
+        {
+            int count = richTextBox1.Lines.Count();
+
+            for (int i = 0; i < count; i++)
+            {
+                if (i > 4)
+                {
+                    label1.Text += ("DOZWOLONE MAX. 5 LINII! POPRAW TEKST!");
+                    break;
+                }
+
+                label1.Text += ($"W {i + 1} linii jest {richTextBox1.Lines[i].Length} znaków. ");
+
+                if (richTextBox1.Lines[i].Length > 15)
+                    label1.Text += "ZA DUŻO ZNAKÓW!!!";
+
+                label1.Text += ("\n");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = previusText;
+        }
+
+        
     }
 }
