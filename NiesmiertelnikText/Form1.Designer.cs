2aed58a0b8d17c02780e28bebe3fc8786b2897b3
Marcin N <mnawrocki81@gmail.com>
2019-07-28 23:45:39 +0200

    Zmiana ikony aplikacji

--- NiesmiertelnikText/Form1.Designer.cs
+++ NiesmiertelnikText/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -143,6 +144,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Nieśmiertelnik Tekst";
             this.groupBox1.ResumeLayout(false);
